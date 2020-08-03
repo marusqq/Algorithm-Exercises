a040a76e49577517d8821e7d93e918d0d44804a7
marusqq <pozniakovui@gmail.com>
2020-08-03 14:39:15 +0300

    Submitting this, cause leetcode doesnt accept slices somehow

--- Python/Top-Interview-Questions/Easy-Collection/Array/3-Rotate-List/main.py
+++ Python/Top-Interview-Questions/Easy-Collection/Array/3-Rotate-List/main.py
@@ -1,26 +1,32 @@
 class Solution:
 
-    def reverse(self, list, start, finish) -> list:
+    def old_reverse(self, list, start, finish) -> list:
         if start is None and finish is None:
-            return list[::-1]
+            list = list[::-1]
         elif start == 0:
-            return list[start:finish][::-1] + list[finish:]
+            list = list[start:finish][::-1] + list[finish:]
         else:
-            return list[:start] + list[start:finish][::-1]
+            list = list[:start] + list[start:finish][::-1]
 
-
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
 
     def rotate(self, nums, k: int) -> None:
 
         length_num = len(nums)
         k = k % length_num
 
-        nums = self.reverse(nums, None, None)
-        nums = self.reverse(nums, 0, k - 1)
-        nums = self.reverse(nums, k, length_num)
+        # self.old_reverse(nums, None, None)
+        # self.old_reverse(nums, 0, k - 1)
+        # self.old_reverse(nums, k, length_num)
+        self.reverse(nums, 0, length_num - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, length_num - 1)
 
-        return nums
+        #return nums
 
-ans = Solution()
-list_of_num = [1,2,3,4,5]
-ans.rotate(list_of_num, 1)
+# ans = Solution()
+# list_of_num = [1,2,3,4,5]
+# ans.rotate(list_of_num, 1)
